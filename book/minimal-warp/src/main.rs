781af8c426aa8827dd1c3917e3ef29f81a0ed5fc
Alexander Kobjolke <me@failco.de>
2023-07-17 22:23:17 +0200

    Add POST handler to add questions

--- book/minimal-warp/src/main.rs
+++ book/minimal-warp/src/main.rs
@@ -121,6 +121,18 @@ async fn get_questions(
     }
 }
 
+async fn add_question(
+    store: Store,
+    question: Question,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    store
+        .questions
+        .write()
+        .await
+        .insert(question.id.clone(), question);
+    Ok(warp::reply::with_status("Question added", StatusCode::OK))
+}
+
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
     if let Some(error) = r.find::<CorsForbidden>() {
         Ok(warp::reply::with_status(
@@ -152,15 +164,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .allow_any_origin()
         .allow_header("content-type")
         .allow_methods(&[Method::PUT, Method::GET, Method::POST, Method::DELETE]);
-    let get_items = warp::get()
+
+    let get_questions = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and(warp::query())
-        .and(store_filter)
-        .and_then(get_questions)
-        .recover(return_error);
+        .and(store_filter.clone())
+        .and_then(get_questions);
 
-    let routes = get_items.with(cors);
+    let add_question = warp::post()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and(warp::body::json())
+        .and_then(add_question);
+
+    let routes = get_questions
+        .or(add_question)
+        .with(cors)
+        .recover(return_error);
 
     warp::serve(routes).run(([127, 0, 0, 1], 1337)).await;
 
